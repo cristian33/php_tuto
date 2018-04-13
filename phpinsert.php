b63cfd698c6cc562187564d1efb05e8804a157c7
cristian <cristianquevedo103@hotmail.com>
2018-04-13 12:48:51 -0300

    phpinsert cambie linea 9

--- phpinsert.php
+++ phpinsert.php
@@ -2,10 +2,10 @@
  include ('conex.php');
 if (isset($_POST['nombre'] )&& !empty($_POST['nombre']) && isset($_POST ['pw'] )&& !empty( $_POST['pw']) ){
    $con= mysql_connect($host, $user, $pw) or die ("problemas al conectar");
-   mysql_select_db("tuto", $con) or die("problema al conectar la base de datos");
+   mysql_select_db("tuto", $con) or die("problema al conectar la data base");
    echo ' ejecuto'."<br/> $_POST[nombre]',' $_POST[pw]";
    mysql_query("insert into pasw (nombre, password ) values('$_POST[nombre]', '$_POST[pw]')", $con);
 } else {
-    echo 'no se ejecuto esto';    
+    echo 'no se ejecuto esto loco';    
 }
 ?>
